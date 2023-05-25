050d51d25b7aae1a67be8e81ccc5a156f816f7e0
Cezar Hirsescu <cezar.hirsescu@gmail.com>
2023-05-25 11:33:47 -0400

    Changes nav tab color

--- Navigation.js
+++ Navigation.js
@@ -1,60 +1,61 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text } from "react-native"
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-// import SignInScreen from "../screens/SignInScreen";
-// import SignUpScreen from "../screens/SignUpScreen";
-// import ConfirmSignUpScreen from "../screens/ConfirmSignUpScreen";
-// import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
-// import NewPasswordScreen from "../screens/NewPasswordScreen";
-// import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen"
+import SignUpScreen from "./screens/SignUpScreen"
+import ConfirmSignUpScreen from "./screens/ConfirmSignUpScreen"
+import HomeScreen from "./screens/HomeScreen"
+import ChatScreen from "./screens/ChatScreen"
 
-import LoginScreen from "./screens/LoginScreen";
-import SignUpScreen from "./screens/SignUpScreen";
-import ConfirmSignUpScreen from "./screens/ConfirmSignUpScreen";
-import HomeScreen from "./screens/HomeScreen";
-import ChatScreen from "./screens/ChatScreen";
+import LoadingScreen from "./screens/LoadingScreen"
+import ProfileScreen from "./screens/ProfileScreen"
 
-import LoadingScreen from "./screens/LoadingScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import { COLORS } from "./constants"
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 function Tabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Settings" component={SignUpScreen} />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: { backgroundColor: COLORS.darkGray },
+			}}
+		>
+			<Tab.Screen name="Chat" component={ChatScreen} />
+			<Tab.Screen name="Settings" component={SignUpScreen} />
+		</Tab.Navigator>
+	)
 }
 
 const Navigation = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+				{/* <Stack.Screen
           name="LoadingScreen"
           component={LoadingScreen}
         ></Stack.Screen> */}
-        <Stack.Screen name="LoginScreen" component={LoginScreen}></Stack.Screen>
-        <Stack.Screen name="Tabs" component={Tabs}></Stack.Screen>
-        <Stack.Screen
-          name="RegisterScreen"
-          component={SignUpScreen}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="RegisterConfirmScreen"
-          component={ConfirmSignUpScreen}
-        ></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+				<Stack.Screen name="LoginScreen" component={LoginScreen}></Stack.Screen>
+				<Stack.Screen name="Tabs" component={Tabs}></Stack.Screen>
+				<Stack.Screen
+					name="RegisterScreen"
+					component={SignUpScreen}
+				></Stack.Screen>
+				<Stack.Screen
+					name="RegisterConfirmScreen"
+					component={ConfirmSignUpScreen}
+				></Stack.Screen>
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+}
 
-export default Navigation;
+export default Navigation
