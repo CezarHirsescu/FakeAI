66e4c73dd39d999d3ccdfd56e45aa24da9f85196
335093894 <132513234+335093894@users.noreply.github.com>
2023-05-30 16:31:26 -0400

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,7 +1,8 @@
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
 import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import LoginScreen from "./screens/LoginScreen"
 import SignUpScreen from "./screens/SignUpScreen"
@@ -42,8 +43,33 @@ function Tabs() {
 				tabBarStyle: { backgroundColor: COLORS.darkGray },
 			}}
 		>
-			<Tab.Screen name="Home" component={Home} />
-			<Tab.Screen name="Profile" component={ProfileScreen} />
+			<Tab.Screen
+				name="Home"
+				component={Home}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+					<MaterialCommunityIcons name="home" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Chat"
+				component={ChatScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+					<MaterialCommunityIcons name="chat" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Profile"
+				component={ProfileScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+					<MaterialCommunityIcons name="account" color={color} size={size} />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	)
 }
